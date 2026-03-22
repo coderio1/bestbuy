e774520f2ea63e372831d895d31806c730aa3bdb
Marko Roncevic <seal@R6.local>
2026-03-23 00:33:07 +0100

    create product class

---
+++ products.py
@@ -0,0 +1,57 @@
+class Product:
+    def __init__(self, name: str, price: float, quantity: int):
+        if not name:
+            raise ValueError("Name cannot be empty.")
+        if price < 0:
+            raise ValueError("Price cannot be negative.")
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative.")
+        self.name: str = name
+        self.price: float = price
+        self.quantity: int = quantity
+        self.active: bool = True
+
+    def get_quantity(self) -> int:
+        """Returns product quantity."""
+        return self.quantity
+
+    def set_quantity(self, quantity: int):
+        """Sets product quantity"""
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+    def is_active(self) -> bool:
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity: int) -> float:
+        if quantity <= 0:
+            raise ValueError("Purchase quantity must be greater than 0.")
+        if not self.active:
+            raise Exception("Product is not active.")
+        if quantity > self.quantity:
+            raise Exception("Not enough stock available.")
+        self.set_quantity(self.quantity - quantity)
+        return self.price * quantity
+
+# bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+# mac = Product("MacBook Air M2", price=1450, quantity=100)
+#
+# print(bose.buy(50))
+# print(mac.buy(100))
+# print(mac.is_active())
+#
+# bose.show()
+mac.show()
+
+bose.set_quantity(1000)
+bose.show()
